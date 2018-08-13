79e3c57fa3aad3872e5c5e69d00fa37c7f72b404
postolenko <denis.postolenko@gmail.com>
2018-08-13 22:44:01 +0300

    V podarok

--- js/scripts.js
+++ js/scripts.js
@@ -35,7 +35,54 @@ $(document).ready(function() {
 
 	$(".svg-text p").text("");
 
-	getStr();	
+	getStr();
+
+	$(".show_popup").click(function(e) {
+
+        e.preventDefault();
+
+        popupName = $(this).attr("data-popup-name");
+        popupBlock = $("[data-popup = '"+ popupName +"']");
+
+        popupBlock.fadeIn(400);
+
+    });
+
+     $(this).keydown(function(eventObject){
+
+        if (eventObject.which == 27) {
+
+            if ( $(".popup_wrapp").is(":visible") ) {
+
+                $(".popup_wrapp").fadeOut(300);
+
+            }
+
+        }
+
+    });
+
+    $(".close-popup").click(function() {
+
+        popupBlock = $(this).closest(".popup_wrapp");
+
+        popupBlock.fadeOut(300);
+
+    });
+
+
+    $(document).mouseup(function (e){
+
+        hide_element = $('.popup');
+
+        if (!hide_element.is(e.target)
+
+            && hide_element.has(e.target).length === 0) {
+
+            hide_element.closest(".popup_wrapp").fadeOut(300);
+        }
+
+    });
 
 });
 
